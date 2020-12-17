f8d3b338c479ad48a04eda40ea28031ba135a5d0
Calterat <Calterat@gmail.com>
2020-12-17 09:59:54 -0600

    added some comments

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,12 +1,16 @@
 const path = require('path');
+// imports router method from Express
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
+// spits out the index html file on root
+router.get('/', (_req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
 })
 
-router.get('/notes', (req, res) => {
+// spits out the notes html file on /notes
+router.get('/notes', (_req, res) => {
   res.sendFile(path.join(__dirname, '../../public/notes.html'));
 })
 
+// exports these router apis.
 module.exports = router;
--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const apiRoutes = require('./routes/apiRoutes');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// use all assests in the public folder
+// use all assests in the public folder for use with the htmlRoutes
 app.use(express.static('public'));
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// uses the router on the html distributor
 app.use('/', htmlRoutes);
+// uses the router on the api distributor
 app.use('/api', apiRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
