86d25483aa8485013f479d5c8fcd52789e105767
Calterat <Calterat@gmail.com>
2020-12-16 18:24:57 -0600

    added express and listen

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const { notes } = require('./db/db');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.get('/api/notes', (req, res) => {
+  console.log(req.query);
+  res.json(notes);
+})
+
+app.listen(3001, () => {
+  console.log(`API server now on port ${PORT}!`);
+})
